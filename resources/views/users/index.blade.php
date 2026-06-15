100eb55bc334a6074f39d1e5a11d99fa2b6ec39c
anjana123g <emailtoanjanagopinathan@gmail.com>
2026-06-15 17:47:41 +0530

    commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,47 @@
+@extends('adminlte::page')
+
+@section('title', 'User Management')
+
+@section('content')
+<div class="container-fluid">
+    <h1>User Management</h1>
+    <div class="card-body p-4" id="card">
+        <form method="POST" action="#">
+            @csrf
+            <div class="mb-3">
+                <label class="form-label fw-semibold">Name</label>
+                <input type="text" name="name" class="form-control" placeholder="Enter the Name">
+            </div>
+            <div class="mb-3">
+                <label class="form-label fw-semibold">Email</label>
+                <input type="text" name="email" class="form-control" placeholder="Enter the Email">
+            </div>
+            <div class="mb-3">
+                <label class="form-label fw-semibold">Phone</label>
+                <input type="number" name="phone" class="form-control" placeholder="Enter the Phone">
+            </div>
+            <div class="mb-3">
+                <label class="form-label fw-semibold">Category</label>
+                <select name="role" class="form-control">
+                    <option value="manager">Manager</option>
+                    <option value="supplier">Supplier</option>
+                    <option value="staff">Staff</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label class="form-label fw-semibold">Status</label>
+                <select name="status" class="form-control rounded">
+                    <option value="active">Active</option>
+                    <option value="inactive">Inactive</option>
+                </select>
+            </div>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary btn-lg">
+                    Save Product
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserManagementController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
-
+Route::middleware(['auth'])->group(function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/users', [UserManagementController::class, 'index'])->name('users.index');
+});
